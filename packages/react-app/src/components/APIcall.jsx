e1cf94d559ab98f13a57171ccaa0780cfd37342e
babivillanova <villanovaarch@gmail.com>
2022-11-07 23:30:55 -0300

    express and first route

---
+++ packages/react-app/src/components/APIcall.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class APIcall extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div>
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+}
+
+export default APIcall;
--- packages/react-app/src/views/Dashboard.jsx
+++ packages/react-app/src/views/Dashboard.jsx
@@ -1,8 +1,9 @@
 import { useContractReader } from "eth-hooks";
 import { ethers } from "ethers";
-import React from "react";
+import React, { Suspense } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import APIcall from "../components/APIcall";
 
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
@@ -11,7 +12,13 @@ import "./Home.css";
  * @returns react component
  **/
 function Dashboard() {
-  return <div>dsds</div>;
+  return (
+    <div className="container">
+      <div>dsds</div>
+
+      <APIcall />
+    </div>
+  );
 }
 
 export default Dashboard;
