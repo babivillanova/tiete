649a80597d39c8ca757cce457ef45a25b1867b5c
Barbara Villanova <villanovaarch@gmail.com>
2022-11-11 20:58:42 -0300

    add api inside

--- packages/react-app/src/components/Spotify.jsx
+++ packages/react-app/src/components/Spotify.jsx
@@ -1,22 +1,35 @@
 import { Button } from "antd";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useThemeSwitcher } from "react-css-theme-switcher";
 
 export default function Spotify() {
-  const { currentTheme } = useThemeSwitcher();
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // //set isLoggedIn to false
+  // //create a function to set isLoggedIn to true
+  // const login = () => {
+  //   window.open("https://supporting-fan.vercel.app/login", "_blank");
+  //   //get info from window and set isLoggedIn to true
+  //   setIsLoggedIn(true);
+  //   //get info from
+  // };
+  // console.log(isLoggedIn);
 
   return (
     <div style={{ display: "flex" }}>
-      <Button
+      {/* <Button
         style={{ marginLeft: 5 }}
         shape="round"
         onClick={() => {
           //access link to https://supporting-fan.vercel.app/login
-          window.location.href = "https://supporting-fan.vercel.app/login";
+          //open new window to https://supporting-fan.vercel.app/login
+          // change the state of the user to logged in
+          login();
+          console.log(isLoggedIn);
+          // window.location.href = "https://supporting-fan.vercel.app/login";
         }}
       >
         Connect to Spotify
-      </Button>
+      </Button> */}
     </div>
   );
 }
--- packages/react-app/src/views/Dashboard.jsx
+++ packages/react-app/src/views/Dashboard.jsx
@@ -1,9 +1,10 @@
 import { useContractReader } from "eth-hooks";
 import { ethers } from "ethers";
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 import APIcall from "../components/APIcall";
+import { Button } from "antd";
 
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
@@ -11,14 +12,52 @@ import APIcall from "../components/APIcall";
  * @param {*} readContracts contracts from current chain already pre-loaded using ethers contract module. More here https://docs.ethers.io/v5/api/contract/contract/
  * @returns react component
  **/
-function Dashboard() {
-  return (
-    <div className="container">
-      <div>dsds</div>
 
-      <APIcall />
-    </div>
-  );
+function Dashboard() {
+  const CLIENT_ID = "34c24373d1af48428d219f760ee07384";
+  const REDIRECT_URI = "http://localhost:3000/dashboard/";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find(elem => elem.startsWith("access_token"))
+        .split("=")[1];
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+      setToken(token);
+    }
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+    window.localStorage.clear();
+  };
+
+  if (token !== "") {
+    return (
+      <div className="container3">
+        <Button onClick={logout}>Logout</Button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+          Login to Spotify
+        </a>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
